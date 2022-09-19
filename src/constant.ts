0c4947b63dee3f95c8d278e57a6354add3f0c3ef
Max <max@ref.finance>
2022-09-19 21:45:00 +0800

    update config by network

--- src/constant.ts
+++ src/constant.ts
@@ -3,14 +3,38 @@ export const FEE_DIVISOR = 10000;
 export const STABLE_LP_TOKEN_DECIMALS = 18;
 export const RATED_POOL_LP_TOKEN_DECIMALS = 24;
 
-export const REF_FI_CONTRACT_ID = 'v2.ref-finance.near';
+export function getConfig(env: string | undefined = process.env.NEAR_ENV) {
+  switch (env) {
+    case 'mainnet':
+      return {
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        walletUrl: 'https://wallet.near.org',
+        WRAP_NEAR_CONTRACT_ID: 'wrap.near',
+        REF_FI_CONTRACT_ID: 'v2.ref-finance.near',
+      };
+    case 'testnet':
+      return {
+        networkId: 'testnet',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        walletUrl: 'https://wallet.testnet.near.org',
+        WRAP_NEAR_CONTRACT_ID: 'wrap.testnet',
+        REF_FI_CONTRACT_ID: 'ref-finance-101.testnet',
+      };
+    default:
+      return {
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        walletUrl: 'https://wallet.near.org',
+        REF_FI_CONTRACT_ID: 'v2.ref-finance.near',
+        WRAP_NEAR_CONTRACT_ID: 'wrap.near',
+      };
+  }
+}
 
-export const config = {
-  networkId: 'mainnet',
-  nodeUrl: 'https://rpc.mainnet.near.org',
-  walletUrl: 'https://wallet.near.org',
-  WRAP_NEAR_CONTRACT_ID: 'wrap.near',
-};
+export const config = getConfig();
+
+export const REF_FI_CONTRACT_ID = config.REF_FI_CONTRACT_ID;
 
 export const STORAGE_TO_REGISTER_WITH_MFT = '0.1';
 
