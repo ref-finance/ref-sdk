64f5be0ab3b203e95216bf64cd460cd6a1d2773f
Max <max@ref.finance>
2023-01-28 00:19:23 +0800

    update mainnet contract address

--- src/constant.ts
+++ src/constant.ts
@@ -24,7 +24,7 @@ export function getConfig(
         REF_TOKEN_ID: 'token.v2.ref-finance.near',
         indexerUrl: 'https://indexer.ref.finance',
         explorerUrl: 'https://testnet.nearblocks.io',
-        REF_DCL_SWAP_CONTRACT_ID: 'dcl.ref-labs.near',
+        REF_DCL_SWAP_CONTRACT_ID: 'dclv2.ref-labs.near',
       };
     case 'testnet':
       return {
