05870360d9e829de3eeef1b168b4bf230822adab
xieqiancaosissi <nature@ref.finance>
2024-04-07 11:36:28 +0800

    add list-token-2 interface (#29)
    Co-authored-by: xieqian <dss@dsss-MacBook-Pro.local>

--- src/indexer.ts
+++ src/indexer.ts
@@ -1,5 +1,8 @@
 import { config, WRAP_NEAR_CONTRACT_ID, NEAR_META_DATA } from './constant';
-import { REF_WIDGET_ALL_TOKENS_LIST_KEY } from './swap-widget/constant';
+import {
+  REF_WIDGET_ALL_TOKENS_LIST_KEY,
+  REF_WIDGET_ALL_LIGHT_TOKENS_LIST_KEY,
+} from './swap-widget/constant';
 import { REPLACE_TOKENS } from './ref';
 import metaIconDefaults from './metaIcons';
 
@@ -53,6 +56,46 @@ export const getTokens = async (reload?: boolean) => {
             !reload &&
             localStorage.setItem(
               REF_WIDGET_ALL_TOKENS_LIST_KEY,
+              JSON.stringify(res)
+            );
+          return res;
+        });
+};
+export const getTokensTiny = async (reload?: boolean) => {
+  const storagedTokens =
+    typeof window !== 'undefined' && !reload
+      ? localStorage.getItem(REF_WIDGET_ALL_LIGHT_TOKENS_LIST_KEY)
+      : null;
+
+  return storagedTokens
+    ? JSON.parse(storagedTokens)
+    : await fetch(config.indexerUrl + '/list-token-v2', {
+        method: 'GET',
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      })
+        .then(res => res.json())
+        .then(tokens => {
+          const newTokens = Object.values(tokens).reduce(
+            (acc: any, cur: any, i) => {
+              const id = Object.keys(tokens)[i];
+              return {
+                ...acc,
+                [id]: {
+                  ...cur,
+                  id,
+                },
+              };
+            },
+            {}
+          );
+
+          return newTokens;
+        })
+        .then(res => {
+          typeof window !== 'undefined' &&
+            !reload &&
+            localStorage.setItem(
+              REF_WIDGET_ALL_LIGHT_TOKENS_LIST_KEY,
               JSON.stringify(res)
             );
           return res;
