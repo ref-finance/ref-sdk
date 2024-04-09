a879fd80e0ae3244f1aa36c5fb52913ccd01abe9
xieqian <dss@192.168.0.100>
2024-04-09 23:59:14 +0800

    add indexerUrl params to replace server source

--- src/ref.ts
+++ src/ref.ts
@@ -46,11 +46,11 @@ let near = new Near({
   headers: {},
   ...getConfig(),
 });
-export const init_env = (env: string) => {
+export const init_env = (env: string, indexerUrl?: string) => {
   near = new Near({
     keyStore: getKeyStore(),
     headers: {},
-    ...getConfig(env),
+    ...getConfig(env, indexerUrl),
   });
   return switchEnv();
 };
